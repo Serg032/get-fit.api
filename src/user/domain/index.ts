9fbfbd6ce4d61d18b9bbcfcbd78d3aeb41d569f4
Serg032 <sergioradigales@gmail.com>
2024-03-05 21:07:09 +0100

    wip

--- src/user/domain/index.ts
+++ src/user/domain/index.ts
@@ -1,11 +1,9 @@
 export interface User {
-  id: string;
   name: string;
   lastname: string;
   email: string;
   password: string;
   username: string;
-  isActive: boolean;
 }
 
-export type CreateUserCommand = Omit<User, "id" | "isActive">;
+export interface CreateUserCommand extends User {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,22 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { User } from "./domain/user.model";
+import { CreateUserCommand } from "./domain";
 
 @Injectable()
 export class UserService {
   // eslint-disable-next-line no-unused-vars
   constructor(@Inject("USER_REPOSITORY") private userModel: typeof User) {}
-  async register() {
+  async register(command: CreateUserCommand) {
     return await this.userModel.create({
-      name: "Sergio",
+      name: command.name,
+      lastname: command.lastname,
+      email: command.email,
+      username: command.username,
+      password: command.password,
+      isActive: true,
     });
   }
+
   async findAll(): Promise<User[]> {
     return await this.userModel.findAll();
   }
