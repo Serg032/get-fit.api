6a516f90c083c5c9a8288c8e3767ab15df488f25
Serg032 <sergioradigales@gmail.com>
2024-03-05 21:44:48 +0100

    reguster fix

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,14 +7,20 @@ export class UserService {
   // eslint-disable-next-line no-unused-vars
   constructor(@Inject("USER_REPOSITORY") private userModel: typeof User) {}
   async register(command: CreateUserCommand) {
-    return await this.userModel.create({
-      name: command.name,
-      lastname: command.lastname,
-      email: command.email,
-      username: command.username,
-      password: command.password,
-      isActive: true,
-    });
+    try {
+      await this.userModel.create({
+        name: command.name,
+        lastname: command.lastname,
+        email: command.email,
+        username: command.username,
+        password: command.password,
+        isActive: true,
+      });
+
+      return "User registered";
+    } catch (error) {
+      throw error;
+    }
   }
 
   async findAll(): Promise<User[]> {
